88bcf47f6f4d6d050ba853234229a1332fade1f5
Erik Steggall <eqsteggall@gmail.com>
2017-05-23 08:47:06 -0700

    Added run instructions to hello.js

--- src/boiler/javascript/hello.js
+++ src/boiler/javascript/hello.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env nodejs
+/*
+Run: Need two terminals
+
+## First terminal ##
+ # Make file executable
+  chmod +x hello.js
+ # Run script
+  ./hello.js
+
+## Second terminal ##
+ # Get info from the local host on port 8080 
+  curl http://localhost:8080
+
+*/
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
