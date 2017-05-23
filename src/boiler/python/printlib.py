35e22b174b822c7d6ef4fb7d0cc19f49ea54765d
Erik Steggall <eqsteggall@gmail.com>
2017-05-23 09:49:17 -0700

    Added notes to printlib.py

--- src/boiler/python/printlib.py
+++ src/boiler/python/printlib.py
@@ -2,6 +2,10 @@
 This is intended to be a lib for weird print format stuff
 I run into all the time. 
 
+NOTE: You can import this (instructions below) but I 
+usually just use this as a reference since $PATHs can
+be annoying to figure out when deploying on a new sys
+
 Import it as a lib by specifying:
 import /full/path/to/printlib.py
 
