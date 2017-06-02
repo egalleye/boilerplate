9280035ebc93a43ddb53b920e7b8ccb632a78d15
Erik Steggall <eriksteggall@gmail.com>
2017-06-02 16:49:38 -0400

    Changed around a few things to get a more accurate read on the cpu

--- src/boiler/shell/cpu_stats.sh
+++ src/boiler/shell/cpu_stats.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cpu_usage=$(grep 'cpu ' /proc/stat | awk '{cpu_usage=($2+$4)*100/($2+$4+$5)} END {print cpu_usage}')
-echo "CPU usage (%)" > cpu_usage.out 
-echo "$cpu_usage" >> cpu_usage.out
+onehundred="100"
+cpu_usage=$(top -b -n2 -d0.01 | awk '/^top/{i++}i==2' |  grep --line-buffered "Cpu(s)" | awk '{print $8}')
+idstr="id,"
+
+if [ "$cpu_usage" == "$idstr" ]; then
+    exit 1;
+fi
+
+cu=$(bc <<< "$onehundred-$cpu_usage")
+
+echo "$cu"
