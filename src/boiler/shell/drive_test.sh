7dedb2f80458aebfd46f2fcf37e2e73912c7ffb2
Erik Steggall <eqsteggall@gmail.com>
2017-06-07 15:55:51 -0700

    Added drive test

--- src/boiler/shell/drive_test.sh
+++ src/boiler/shell/drive_test.sh
@@ -1,46 +1,62 @@
 #!/bin/bash
 
+
 one="1"
 mnt_dir="/mnt/data/"
-output_dir="/hdwr_test/drives/"
 bigddfile="bigddfile.out"
-tmpfile="tmp.out"
-fileext=".txt"
+outputdir="/home/test/supermicro_benchmarks/drive/"
+ext=".txt"
 
-declare -a drives=("/dev/sda"
-                "/dev/sdb"
-                "/dev/sdc"
-                "/dev/sdd"
-                "/dev/sde"
-                "/dev/sdf"
-                "/dev/sdg"
-                "/dev/sdh"
-                "/dev/sdi"
-                "/dev/sdj"
-                "/dev/sdk"
-                "/dev/sdl"
-                "/dev/sdm"
-                )
+# EQS NOTE: For nvme's we'll need this line instead of the basic /dev/sd*
+#ls_out=$(ls /dev/sd*; ls /dev/nvme*)
+ls_out=$(ls /dev/sd*)
 
+#### Disc test ####
 
-for drive in "${drives[@]}"
+## FIO setup ##
+sudo mkdir -p /mnt/data
+sudo chown -R test:test /mnt/data
+
+for drive in ${ls_out[@]}
 do
-    sudo mount $drive$one $mnt_dir
+    
+    while [ 1 ] 
+    do
+        if ! df -h | grep -q '$mnt_dir'; then
+        	break;
+        else
+        	echo "$drive still mounted"
+                sudo umount -f $mnt_dir
+        	sleep 1
+        fi
+    done
+    sudo umount -f $mnt_dir
     drive_name=$(echo "$drive" | sed -e 's/\/dev\///g')
+    echo "$drive_name"
+    sudo parted -s -a optimal $drive mklabel gpt -- mkpart primary ext4 1 -1
+    sudo mkfs.ext4 $drive$one
 
-#    echo "$drive_name's dd speed:"
+    sudo mount $drive$one $mnt_dir
+    sudo chown -R test:test $mnt_dir
+    ## FIO Test ##
+    echo "################ FIO ################"
+    fio_out=$(fio random_read_test.fio)
+    drive_bandwidth=$(echo "$fio_out" | grep "stdev" | grep "bw" | awk '{print $10}' | sed -e 's/stdev=//g')
+    drive_clat=$(echo "$fio_out" | grep "stdev" | grep "clat" | awk '{print $6}' | sed -e 's/stdev=//g')
 
-    ddspeed=$(sudo dd if=/dev/zero of=$mnt_dir$bigddfile bs=64M count=1 iflag=fullblock |& grep "bytes" | awk '{print $8 $9}' | sed -e 's/\//p/g')
-
-#    echo "$ddspeed"
-    echo "dd_64Mb" > "/home/test/scratch/$tmpfile" 
-    echo "$ddspeed" >> "/home/test/scratch/$tmpfile" 
-
-    ddrandspeed=$(sudo dd if=/dev/zero of=$mnt_dir$bigddfile bs=64M count=64 iflag=fullblock |& grep "bytes" | awk '{print $8 $9}' | sed -e 's/\//p/g')
-    sed_line="sed -e '2s/$/, $ddrandspeed/'"
-    cat "$tmpfile" | sed '1s/$/, dd_4Gb/' | eval $sed_line | tee "$output_dir$drive_name$fileext"
+    echo "################ Cleanup ################"
+    ## Clean up ##
     sudo rm -f "$mnt_dir/*"
     sudo umount -f $mnt_dir
-    #echo "$drive"
-done
+    # Remove Partition 
+    sudo parted -s $drive rm 1
 
+    ## dd test (zeros) ##
+    echo "################ dd tests ################"
+    ddspeed=$(sudo dd if=/dev/zero of=$mnt_dir$bigddfile bs=64M count=1 iflag=fullblock |& grep "bytes" | awk '{print $8 $9}' | sed -e 's/\//p/g')
+    ddfourg_speed=$(sudo dd if=/dev/zero of=$mnt_dir$bigddfile bs=64M count=64 iflag=fullblock |& grep "bytes" | awk '{print $8 $9}' | sed -e 's/\//p/g')
+
+    echo "fio_bandwidth_kbps, fio_clat_usec, dd_64M_speed, dd_4G_speed" > $outputdir$drive_name$ext
+    echo "$drive_bandwidth, $drive_clat, $ddspeed, $ddfourg_speed" >> $outputdir$drive_name$ext
+
+done
