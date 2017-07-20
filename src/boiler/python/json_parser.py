d579b39d7e0fad34f4752245a29c5e45db0f4b33
Erik Steggall <eqsteggall@gmail.com>
2017-07-20 15:22:59 -0700

    Added json_parser.py

--- src/boiler/python/json_parser.py
+++ src/boiler/python/json_parser.py
@@ -1,10 +1,69 @@
 import json
 import pprint 
+import subprocess
+
+
+def verify_motherboard_info(motherboard_info):
+    linecnt = 0
+    motherboard_pn = motherboard_info['mb_pn']
+    motherboard_serial = motherboard_info['mb_serial']
+    # Call grep to get dmidecode info on motherboard
+    try:
+        dmidout=subprocess.check_output('cat dmidecode.out | grep -A 4 "Base Board Information"', shell=True)
+        dmidout=dmidout.decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+    for line in dmidout.split('\n'):
+        line = line.rstrip()
+        if (linecnt == 2):
+            motherboard_prod_num = line.split(':')[1]
+            motherboard_prod_num = motherboard_prod_num.lstrip()
+        elif(linecnt == 4):
+            motherboard_serial_num = line.split(':')[1]
+            motherboard_serial_num = motherboard_serial_num.lstrip()
+        linecnt += 1
+
+    if (motherboard_pn == motherboard_prod_num):
+        print("CHECK: motherboard product number is {0}".format(motherboard_pn))
+    else:
+        print("FAILED: motherboard product number is {0} expected {1}".format(motherboard_prod_num, motherboard_pn))
+
+    if (motherboard_serial == motherboard_serial_num):
+        print("CHECK: motherboard serial number is {0}".format(motherboard_serial))
+    else:
+        print("FAILED: motherboard serial number is {0} expected {1}".format(motherboard_serial_num, motherboard_serial))
+
 
 def verify_sys(sys_info):
+    linecnt = 0
     system_pn = sys_info['system_pn']
     system_serial = sys_info['system_serial']
-    print("sys serial number = " + system_serial)
+    # Call grep to get 4 lines pertaining to system info 
+    try:
+        dmidout=subprocess.check_output('cat dmidecode.out | grep -A 4 "System Information"', shell=True)
+        dmidout=dmidout.decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+    for line in dmidout.split('\n'):
+        line = line.rstrip()
+        if (linecnt == 2):
+            system_prod_num = line.split(':')[1]
+            system_prod_num = system_prod_num.lstrip()
+        elif(linecnt == 4):
+            system_serial_num = line.split(':')[1]
+            system_serial_num = system_serial_num.lstrip()
+        linecnt += 1
+    if (system_pn == system_prod_num):
+        print("CHECK: system product number is {0}".format(system_pn))
+    else:
+        print("FAILED: system product number is {0} expected {1}".format(system_prod_num, system_pn))
+
+    if (system_serial == system_serial_num):
+        print("CHECK: system serial number is {0}".format(system_serial))
+    else:
+        print("FAILED: system serial number is {0} expected {1}".format(system_serial_num, system_serial))
 
 """
 verify_system() -- break system into hardware categories and call respective tests
@@ -17,6 +76,7 @@ def verify_system(system_conf):
     # pprint.pprint(system_conf)
     # 
     sys_info = system_conf['system']
+    motherboard_info = system_conf['motherboard']
 
     mb_pn = system_conf['motherboard']['mb_pn']
     mb_serial = system_conf['motherboard']['mb_serial']
@@ -27,6 +87,7 @@ def verify_system(system_conf):
     cpu_pn = system_conf['cpu']['cpu_pn']
 
     verify_sys(sys_info)
+    verify_motherboard_info(motherboard_info)
 
 """
     for memory_device in system_conf['memory']:
